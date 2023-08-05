677a50e5115a5bc014b337ad17dc4528e2ed36b7
Nick Ilin <ninikitos90@gmail.com>
2023-08-05 15:30:37 -0400

    fixed desktop menu items and adtech img

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@ const mobileMenu = document.querySelector(".nav__nav-links-mobile");
 const menu = document.querySelector(".nav__hamburger");
 const menuBackground = document.querySelector(".mobile-background");
 
-
-
-// menuBackground.addEventListener('click', () => {
-//   mobileMenu.classList.remove("show-background");
-// })
-
 menu.addEventListener('click', () => {
   mobileMenu.classList.add("show-menu");
   menuBackground.classList.add("show-background");
